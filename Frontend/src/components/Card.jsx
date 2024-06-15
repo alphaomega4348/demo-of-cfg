d7c2a70d11a8eec81fd841fc543e065f32619c7a
Shivam Kumar <117167192+alphaomega4348@users.noreply.github.com>
2024-06-16 03:06:36 +0530

    card updated for classes

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,18 +1,31 @@
-import { Link } from 'react-router-dom';
-
-const Card = () => {
+const Card = ({ className, classId}) => {
   return (
-    <Link to="/table">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white flex flex-col cursor-pointer">
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">CLASS 1</div>
-          <p className="text-gray-700 text-base">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat vel quas incidunt blanditiis quisquam eos tempora recusandae iusto nemo quidem! Voluptas repellat ad itaque nisi, rem dolorum quis expedita! Ipsam beatae, provident consequatur explicabo assumenda quasi autem dolor accusamus cumque asperiores velit inventore quae. Repellendus perspiciatis commodi fuga vitae, in cupiditate odio ullam eligendi. Temporibus, fugiat?
-          </p>
-        </div>
-      </div>
-    </Link>
-  )
-}
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '1px solid #ddd',
+      borderRadius: '20px',
+      backgroundColor: '#f9eaf7', // light pink-purple shade
+      margin: '20px',
+      padding: '20px',
+      boxShadow: '0 10px 20px 0 rgba(0,0,0,0.2)',
+      transition: '0.3s',
+    }}>
+      <h2 style={{ 
+        color: '#6a097d', // dark purple text
+        fontFamily: 'Arial, sans-serif',
+        fontSize: '24px',
+        fontWeight: 'bold',
+        marginBottom: '20px',
+      }}>
+        {className}
+      </h2>
+      
+     
+    </div>
+  );
+};
 
-export default Card
+export default Card;
