b1cb6632c2bfe0645ea3f55033174598ec416f55
Shivam Kumar <117167192+alphaomega4348@users.noreply.github.com>
2024-06-15 22:05:31 +0530

    home pafe

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+// Home.jsx
+import Card from "../components/Card"
 
 const Home = () => {
-  return <div className="text-center font-bold text-5xl">Hello World</div>;
-};
+  const cards = [1, 2, 3]; // replace this with your actual data
 
-export default Home;
+  return (
+    <div className="p-10">
+        <div className="flex justify-between items-center">
+            <h1 className="text-4xl font-bold text-blue-600">Vowels of People Association</h1>
+            <div className="">
+                <img className="h-16 w-16 rounded-full" src="https://avatar.iran.liara.run/public"  alt="kjsadkl" />
+            </div>
+        </div>
+        <div className="flex flex-wrap justify-center md:justify-start">
+          {cards.map((card, index) => (
+            <div className="w-full md:w-1/3 px-2" key={index}>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              
+            </div>
+          ))}
+        </div>
+    </div>
+  )
+}
+
+export default Home
